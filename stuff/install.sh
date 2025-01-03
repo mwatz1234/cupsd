dcd974831fe371b2d03f15803d78895ad83ff3bf
mwatz1234 <87551985+mwatz1234@users.noreply.github.com>
2025-01-02 18:22:40 -0800

    Update install.sh

--- stuff/install.sh
+++ stuff/install.sh
@@ -1,3 +1,11 @@
+#Copy the conf files if they do not exist
 mkdir -p /etc/cups/printers
 cp -n /temp/printers.conf /etc/cups/printers.conf
 cp -n /temp/cupsd.conf /etc/cups/cupsd.conf
+#Remove the temp directory
+rm -rf /temp/
+
+#soft link the 2 .conf files
+mkdir -p /etc/cups/config
+ln -s /etc/cups/printers.conf /etc/cups/config/printers.conf
+ln -s /etc/cups/cupsd.conf /etc/cups/config/cupsd.conf
